f323f56bd838e3a63df068487dbf2b500d9b1c19
Anawat Muangjai <anawat.muangjai@gmail.com>
2017-09-29 16:47:40 +0700

    Create ObservableObjectTests and ObservableObject class

---
+++ CRUD/CRUD.Tests/ObservableObjectTests.cs
@@ -0,0 +1,52 @@
+﻿using CRUD.Data;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUD.Tests
+{
+    [TestClass]
+    public class ObservableObjectTests
+    {
+        [TestMethod]
+        public void PropertyChangedEventHandlerisRaised()
+        {
+            var obj = new StubObservableObject();
+
+            bool raised = false;
+
+            obj.PropertyChanged += (sender, e) =>
+                                {
+                                    Assert.IsTrue(e.PropertyName == "ChangedProperty");
+                                    raised = true;
+                                };
+
+            obj.ChangedProperty = "Some value";
+
+            if (!raised) Assert.Fail("PropertyChanged was never invoked.");
+
+        }
+
+        class StubObservableObject : ObservableObject
+        {
+            private string _changedProperty;
+
+            public string ChangedProperty
+            {
+                get { return _changedProperty; }
+                set
+                {
+                    _changedProperty = value;
+                    NotifyPropertyChanged();
+                }
+            }
+
+
+        }
+
+
+    }
+}
