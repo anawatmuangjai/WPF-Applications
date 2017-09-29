4c917b4c1e15b1a10417f399779f68dbe5b1a248
Anawat Muangjai <anawat.muangjai@gmail.com>
2017-09-29 17:25:55 +0700

    Create ViewModelTests and ViewModel class

---
+++ CRUD/CRUD.Data/ViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUD.Data
+{
+    public abstract class ViewModel : IDataErrorInfo
+    {
+        public string this[string columnName] => throw new NotImplementedException();
+
+        public string Error => throw new NotImplementedException();
+    }
+}
---
+++ CRUD/CRUD.Tests/ViewModelTests.cs
@@ -0,0 +1,29 @@
+﻿using CRUD.Data;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUD.Tests
+{
+    [TestClass]
+    public class ViewModelTests
+    {
+        [TestMethod]
+        public void IsAbstractBaseClass()
+        {
+            Type t = typeof(ViewModel);
+
+            Assert.IsTrue(t.IsAbstract);
+        }
+
+        [TestMethod]
+        public void IsIDataErrorInfo()
+        {
+            Assert.IsTrue(typeof(IDataErrorInfo).IsAssignableFrom(typeof(ViewModel)));
+        }
+    }
+}
